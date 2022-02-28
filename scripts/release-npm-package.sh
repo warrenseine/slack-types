fd54ffb222e746c0cf706e772d7f7bb62926c675
Warren Seine <warren.seine@onfido.com>
2022-02-28 12:22:38 +0100

    🩹 Fix release process

--- scripts/release-npm-package.sh
+++ scripts/release-npm-package.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-cd `dirname $0`/..
+[[ "$npm_package_name" == "slack-types" ]] || {
+    >&2 echo "error: run with npm (npm run release)"
+    exit 1
+}
+
 npm install
+npm run build
+dirs=$(ls typescript)
+mv typescript/* .
 npm publish
+mv $dirs typescript
